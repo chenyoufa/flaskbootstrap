a9031ae5bec46be1804b8e68a6128701c8d9e14d
Azrael <249600720@qq.com>
2020-11-12 19:25:10 +0800

    服务器信息获取+1

--- utils/ServerInfo.py
+++ utils/ServerInfo.py
@@ -1,4 +1,4 @@
-import psutil
+import psutil,socket,re,requests,platform
 class serverinfo():
     def get_cpu_info(self):
         size_cpu = psutil.cpu_count()
@@ -8,3 +8,17 @@ class serverinfo():
         v_virtual_memory = psutil.virtual_memory()
         size_memory = v_virtual_memory.total/1024/1024
         used_memory = v_virtual_memory.used/1024/1024
+
+    def get_hostname_info(self):
+        host_name = socket.gethostname()
+
+    def get_ip_info(self):
+        html_text = requests.get("https://ip.cn/").text
+        ip_text = re.search(u"<p>您现在的 IP：<code>(.*?)</code></p>", html_text)
+        out_ip = ip_text.group(1)
+        in_ip = socket.gethostbyname(socket.gethostname())
+
+    def get_system_info(self):
+        System_version = platform.platform()
+
+
