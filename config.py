d5e675b298d7daee3580af8dc8e7467b09961fbc
chenyoufa <879756530@qq.com>
2020-11-23 23:13:28 +0800

    菜单功能和模板

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import datetime
 #线上数据库链接
-# SQLALCHEMY_DATABASE_URI = 'mysql://mytest:123456@192.168.1.17:25481/mytest'
+SQLALCHEMY_DATABASE_URI = 'mysql://mytest:123456@58.250.251.12:35481/mytest'
 #本地数据库链接
-SQLALCHEMY_DATABASE_URI = 'mysql://mytest:123456@192.168.1.17/mytest'
+# SQLALCHEMY_DATABASE_URI = 'mysql://mytest:123456@192.168.1.17/mytest'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 #获取web运行开始时间
