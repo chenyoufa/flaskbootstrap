0d5c00a290610547b209b92d4c7e9810c26e5b37
chenyoufa <879756530@qq.com>
2020-12-04 15:44:11 +0800

    11

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ SQLALCHEMY_DATABASE_URI = 'mysql://mytest:123456@58.250.251.12:35481/mytest'
 #本地数据库链接
 # SQLALCHEMY_DATABASE_URI = 'mysql://mytest:123456@192.168.1.17/mytest'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
+# 日志路径配置
+LOG_PATH= "./logs"
 #获取web运行开始时间
 filename = 'test.txt'
 with open(filename, 'w') as file_object:
---
+++ utils/Log.py
@@ -0,0 +1,23 @@
+import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+
+class AppLog:
+    @staticmethod
+    def init_log(path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+        formatter = "\033[am %(asctime)s %(filename)15s:%(lineno)-5d %(funcName)5s() %(levelname)6s\033" \
+                    "[bm : %(message)s"
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format=formatter,
+            datefmt='%a, %d %b %Y %H:%M:%S',
+        )
+        handler = TimedRotatingFileHandler(path + '/flask.log', when="d",
+                                           backupCount=7)
+        handler.setFormatter(logging.Formatter(formatter))
+        handler.setLevel(logging.DEBUG)
+        logging.getLogger('').addHandler(handler)
+        logging.getLogger('sqlalchemy.engine').addHandler(handler)
